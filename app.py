4a17e3181fec3271aea2dd7881b3ce50cb99707e
johngalimi <galimijohn@gmail.com>
2020-02-22 21:15:25 -0500

    parsing time and other fields

--- app.py
+++ app.py
@@ -4,42 +4,54 @@ import pprint
 import requests
 from secrets.token import token
 
-endpoint = "https://api.spotify.com/v1/me/player/currently-playing?market=ES"
+CURRENTLY_PLAYING_ENDPOINT = "https://api.spotify.com/v1/me/player/currently-playing?market=ES"
 
-headers = {
+CURRENTLY_PLAYING_HEADERS = {
     "Accept": "application/json",
     "Content-Type": "application/json",
     "Authorization": "Bearer {}".format(token)
 }
 
-r = requests.get(endpoint, headers=headers)
 
-status = r.status_code
+def get_current_song():
 
-if status == 204:
-    print('no track playing')
+    r = requests.get(CURRENTLY_PLAYING_ENDPOINT, headers=CURRENTLY_PLAYING_HEADERS)
 
-elif status == 200:
+    status = r.status_code
 
-    data = r.json()
-    entry = {}
+    if status == 204:
+        print('no track playing')
 
-    entry['album_id'] = data['item']['album']['id']
-    entry['album_name'] = data['item']['album']['name']
- 
-    artists = data['item']['artists']
-    primary_artist = artists.pop(0)
+    elif status == 200:
 
-    features = '/'.join([artist['name'] for artist in artists])
+        data = r.json()
+        entry = {}
+
+        entry['play_timestamp'] = data['timestamp']
+        entry['duration_elapsed'] = data['progress_ms'] / data['item']['duration_ms']
+
+        entry['album_id'] = data['item']['album']['id']
+        entry['album_name'] = data['item']['album']['name']
+
+        artists = data['item']['artists']
+        primary_artist = artists.pop(0)
+
+        features = '/'.join([artist['name'] for artist in artists])
     
-    entry['artist_id'] = primary_artist['id']
-    entry['artist_name'] = primary_artist['name']
+        entry['artist_id'] = primary_artist['id']
+        entry['artist_name'] = primary_artist['name']
 
-    entry['features'] = features
+        entry['features'] = features
    
-    entry['song_id'] = data['item']['id']
-    entry['song_name'] = data['item']['name']
+        entry['song_id'] = data['item']['id']
+        entry['song_name'] = data['item']['name']
 
-    print(entry)
-else:
-    print('error requesting song')
+        pprint.pprint(data)
+        print('**********')
+        print(entry)
+    else:
+        print('error requesting song')
+
+
+if __name__ == '__main__':
+    get_current_song()
